38b4bd2b70e497b969b3e8a6f4ad00ecc88c2c16
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-06-30 15:04:42 -0400

    docs: fix inline prop description

--- src/plugins/Inline.tsx
+++ src/plugins/Inline.tsx
@@ -5,7 +5,7 @@ import { createModule } from "../core/index.js";
 
 declare module "../types.js" {
     interface LightboxProps {
-        /** HTML div element attributes to be passed to the inline plugin container */
+        /** HTML div element attributes to be passed to the Inline plugin container */
         inline?: React.HTMLAttributes<HTMLDivElement>;
     }
 }
