4ee102da0a2f118a7db4c7e0d41938d40f77e44f
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-07-14 13:56:16 -0400

    fix: streamline portal implementation

--- src/core/modules/Portal.tsx
+++ src/core/modules/Portal.tsx
@@ -4,66 +4,85 @@ import * as ReactDOM from "react-dom";
 import { Component, LightboxDefaultProps } from "../../types.js";
 import { createModule } from "../config.js";
 import { clsx, cssClass, cssVar } from "../utils.js";
-import { useLatest } from "../hooks/useLatest.js";
+import { useLatest, useMotionPreference } from "../hooks/index.js";
 import { useEvents, useTimeouts } from "../contexts/index.js";
 
 export const Portal: Component = ({ children, ...props }) => {
     const [mounted, setMounted] = React.useState(false);
+    const [visible, setVisible] = React.useState(false);
 
     const latestProps = useLatest(props);
+    const latestAnimationDuration = useLatest(!useMotionPreference() ? props.animation.fade : 0);
 
     const { setTimeout } = useTimeouts();
     const { subscribe } = useEvents();
 
-    const [portal] = React.useState(() => {
-        const div = document.createElement("div");
-        div.className = clsx(cssClass("portal"), cssClass("no_scroll_padding"));
+    React.useEffect(() => {
+        setMounted(true);
 
-        const fadeAnimation = latestProps.current.animation.fade;
-        if (fadeAnimation !== LightboxDefaultProps.animation.fade) {
-            div.style.setProperty(cssVar("fade_animation_duration"), `${Math.round(fadeAnimation)}ms`);
-        }
-
-        return div;
-    });
+        return () => {
+            setMounted(false);
+        };
+    }, []);
 
     React.useEffect(
         () =>
             subscribe("close", () => {
-                latestProps.current.on.exiting?.();
+                setVisible(false);
 
-                portal.classList.remove(cssClass("portal_open"));
+                latestProps.current.on.exiting?.();
 
                 setTimeout(() => {
                     latestProps.current.on.exited?.();
 
                     latestProps.current.close();
-                }, latestProps.current.animation.fade);
+                }, latestAnimationDuration.current);
             }),
-        [portal, setTimeout, subscribe, latestProps]
+        [setTimeout, subscribe, latestProps, latestAnimationDuration]
     );
 
-    React.useEffect(() => {
-        document.body.appendChild(portal);
-        setMounted(true);
+    const handlePortalRef = React.useCallback(
+        (node: HTMLDivElement | null) => {
+            if (node) {
+                // reflow
+                node.getBoundingClientRect();
 
-        latestProps.current.on.entering?.();
+                setVisible(true);
 
-        setTimeout(() => {
-            portal.classList.add(cssClass("portal_open"));
-        }, 0);
+                latestProps.current.on.entering?.();
 
-        setTimeout(() => {
-            latestProps.current.on.entered?.();
-        }, latestProps.current.animation.fade);
+                setTimeout(() => {
+                    latestProps.current.on.entered?.();
+                }, latestAnimationDuration.current);
+            }
+        },
+        [setTimeout, latestProps, latestAnimationDuration]
+    );
 
-        return () => {
-            document.body.removeChild(portal);
-            setMounted(false);
-        };
-    }, [portal, setTimeout, latestProps]);
-
-    return ReactDOM.createPortal(mounted ? children : null, portal);
+    return mounted
+        ? ReactDOM.createPortal(
+              <div
+                  ref={handlePortalRef}
+                  className={clsx(
+                      cssClass("portal"),
+                      cssClass("no_scroll_padding"),
+                      visible && cssClass("portal_open")
+                  )}
+                  role="presentation"
+                  aria-live="polite"
+                  {...(props.animation.fade !== LightboxDefaultProps.animation.fade
+                      ? {
+                            style: {
+                                [cssVar("fade_animation_duration")]: `${Math.round(props.animation.fade)}ms`,
+                            },
+                        }
+                      : null)}
+              >
+                  {children}
+              </div>,
+              document.body
+          )
+        : null;
 };
 
 export const PortalModule = createModule("portal", Portal);
