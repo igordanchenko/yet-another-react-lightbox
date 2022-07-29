143d714dcd233680cf3fe274ce5de8ef69e0608f
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-07-29 11:14:03 -0400

    chore: deprecate the useLatest hook

--- src/core/hooks/useLatest.ts
+++ src/core/hooks/useLatest.ts
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+/** @deprecated migrate to useEventCallback */
 export const useLatest = <T>(value: T) => {
     const ref = React.useRef(value);
     ref.current = value;
