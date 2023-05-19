f2cbc3e2421379a723f855bff851a4bead796c41
yanisneverlies <y.vestfalskii@ankr.com>
2023-05-19 13:45:00 +0200

    feat: provide ability to use custom counter separators besides the default one "/"
    feat: provide ability to use custom counter separators besides the default one "/"
    
    fix: introduce `counter.container` prop

---
+++ src/plugins/counter/props.ts
@@ -0,0 +1,11 @@
+import { LightboxProps } from "../../index.js";
+
+export const defaultCounterProps = {
+    separator: "/",
+    container: {},
+} as Required<NonNullable<LightboxProps["counter"]>>;
+
+export const resolveCounterProps = (counter: LightboxProps["counter"]) => ({
+    ...defaultCounterProps,
+    ...counter,
+});
