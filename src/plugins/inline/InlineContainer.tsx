bdaa7f7cbacc628b77a0b252c012a63c460de654
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2023-07-16 11:57:11 -0400

    fix(inline): attach `root` slot styles to the Inline container

--- src/plugins/inline/InlineContainer.tsx
+++ src/plugins/inline/InlineContainer.tsx
@@ -3,9 +3,13 @@ import * as React from "react";
 import { clsx, ComponentProps, cssClass } from "../../index.js";
 
 /** Inline plugin container */
-export function InlineContainer({ inline: { className, ...rest } = {}, children }: ComponentProps) {
+export function InlineContainer({ inline: { className, style, ...rest } = {}, styles, children }: ComponentProps) {
     return (
-        <div className={clsx(cssClass("root"), cssClass("relative"), className)} {...rest}>
+        <div
+            className={clsx(cssClass("root"), cssClass("relative"), className)}
+            style={{ ...styles.root, ...style }}
+            {...rest}
+        >
             {children}
         </div>
     );
