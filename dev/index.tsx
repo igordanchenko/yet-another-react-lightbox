57e0e345bec1dc780dba7409c38ac100c5299a46
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2024-02-17 11:32:50 -0500

    chore: update dev template

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 
 import App from "./App.js";
-import "./index.css";
+import "./styles.css";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
