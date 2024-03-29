98559dced19d07d6326b8af694e88ded40e6cbc2
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2024-03-29 16:33:07 -0400

    fix(inline): fix mouse wheel page scrolling in Safari (fix #256)

--- src/plugins/inline/InlineContainer.tsx
+++ src/plugins/inline/InlineContainer.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
-import { clsx, ComponentProps, cssClass } from "../../index.js";
+import { clsx, ComponentProps, cssClass, cssVar } from "../../index.js";
 
 /** Inline plugin container */
 export function InlineContainer({ inline: { className, style, ...rest } = {}, styles, children }: ComponentProps) {
   return (
     <div
       className={clsx(cssClass("root"), cssClass("relative"), className)}
-      style={{ ...styles.root, ...style }}
+      style={{ [cssVar("controller_overscroll_behavior")]: "contain auto", ...styles.root, ...style }}
       {...rest}
     >
       {children}
